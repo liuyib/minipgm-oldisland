a66bea067e9cffa36dc4b0335f38df9ffe4f9db4
liuyib <1656081615@qq.com>
2021-02-23 17:56:27 +0800

    chore(util): package wx.request

---
+++ utils/request.js
@@ -0,0 +1,109 @@
+const { Base64 } = require('js-base64')
+const { config } = require('../config')
+const { urlResolve } = require('./util')
+
+class HTTP {
+  /**
+   * 封装 wx.request
+   * @api public
+   * @param 见 this._request
+   * @returns {Promise}
+   */
+  request({
+    uri,
+    method = 'GET',
+    data = {},
+    isRefreshToken = true,
+  }) {
+    return new Promise((resolve, reject) => {
+      this._request({
+        uri,
+        method,
+        data,
+        isRefreshToken,
+        resolve,
+        reject,
+      })
+    })
+  }
+
+  /**
+   * 封装 wx.request
+   * @api private
+   * @param {Object} param
+   * @param {string} param.uri             - 请求地址（完整 URL、请求路径）
+   * @param {string} param.method          - 请求方法
+   * @param {Object} param.data            - 传输数据
+   * @param {Function} param.resolve       - 成功的回调
+   * @param {Function} param.reject        - 失败的回调
+   * @param {boolean} param.isRefreshToken - 是否启用无感知刷新 token
+   * @returns
+   */
+  _request({
+    uri,
+    method = 'GET',
+    data = {},
+    // TODO: 无感知获取令牌
+    isRefreshToken = true,
+    resolve,
+    reject,
+  }) {
+    let url = ''
+
+    if (uri.startsWith('http')) {
+      url = uri
+    } else {
+      url = urlResolve(config.apiBaseUrl, uri)
+    }
+
+    const header = {
+      'content-type': 'application/json',
+      Authorization: this._encode(),
+    }
+
+    wx.request({
+      url: url,
+      method,
+      data,
+      header,
+      success: (res) => {
+        const { code, msg } = res.data
+
+        if (code === 0) {
+          resolve(res.data)
+        } else {
+          reject(msg)
+        }
+      },
+      fail: (err) => {
+        reject(err)
+      }
+    })
+  }
+
+  /**
+   * TODO
+   * 无感知刷新用户的登录令牌
+   * @api private
+   * @returns
+   */
+  _refresh() {
+
+  }
+
+  /**
+   * 获取 HTTP Basic Auth 中 Authorization 字段的值
+   * @api private
+   * @returns {string} HTTP 头部中的 Authorization 字段的值
+   */
+  _encode() {
+    const token = wx.getStorageSync('token')
+    const base64 = Base64.encode(`${token}:`)
+    // Authorization: Basic base64(account:secret)
+    return `Basic ${base64}`
+  }
+}
+
+module.exports = {
+  HTTP,
+}
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,8 @@
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : `0${n}`
+}
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -9,11 +14,45 @@ const formatTime = date => {
   return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : `0${n}`
+/**
+ * 删除字符串两边的斜线（包括正斜线 / 和反斜线 \）
+ * @param {string} str 待处理的字符串
+ * @param {Object} [option]            - 参数
+ * @param {Object} [option.left=true]  - 是否删除左端的斜线
+ * @param {Object} [option.right=true] - 是否删除右端的斜线
+ * @example trimSlash('/foo/bar/', { left: false }) => '/foo/bar'
+ * @returns {string} 处理后的字符串
+ */
+const trimSlash = (str, option) => {
+  const op = {
+    left: (option && option.left) || true,
+    right: (option && option.right) || true,
+  }
+  const trimLeft = op.left ? '^[\\\/\\\\]+' : ''
+  const trimRight = op.right ? '[\\\/\\\\]+$' : ''
+  const reg = new RegExp(`${trimLeft}|${trimRight}`, 'g')
+
+  return str.replace(reg, '');
+}
+
+/**
+ * 处理 URL 路径
+ * @param {string} base      - 基础 URL
+ * @param  {...string} paths - 路径
+ * @example urlResolve('https://github.com/v1/', '/signup/') => 'https://github.com/v1/signup/'
+ * @returns {string}
+ */
+const urlResolve = (base, ...paths) => {
+  let result = `${trimSlash(base, { left: false })}/`
+
+  for (const path of paths) {
+    result += `${trimSlash(path)}`
+  }
+
+  return result
 }
 
 module.exports = {
-  formatTime
+  formatTime,
+  urlResolve,
 }
