dd9ccb3fcc6c22af1b04fe92c96a8b417b202489
liuyib <1656081615@qq.com>
2021-02-25 20:08:03 +0800

    feat: add movie component

---
+++ components/classic/movie/index.js
@@ -0,0 +1,30 @@
+// components/classic/movie/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 封面图片
+    banner: {
+      type: String,
+    },
+    // 内容
+    content: {
+      type: String,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
