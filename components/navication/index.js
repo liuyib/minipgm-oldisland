69350e785879fc920f215e0de047d5f654ec6bc7
liuyib <1656081615@qq.com>
2021-02-27 20:59:01 +0800

    feat: add navication component in classic page

---
+++ components/navication/index.js
@@ -0,0 +1,36 @@
+// components/navication/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+    },
+    isFirst: {
+      type: Boolean,
+      value: true,
+    },
+    isLast: {
+      type: Boolean,
+      value: false,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    leftSrc: './assets/arrow-left@black.png',
+    rightSrc: './assets/arrow-right@black.png',
+    leftDisableSrc: './assets/arrow-left@white.png',
+    rightDisableSrc: './assets/arrow-right@white.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
