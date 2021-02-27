60c25e0db08a917b7d33c8bf934c443bbbef46ac
liuyib <1656081615@qq.com>
2021-02-27 14:23:46 +0800

    feat: add order component in classic page

---
+++ components/classic/order/index.js
@@ -0,0 +1,26 @@
+// components/classic/order/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    index: {
+      type: String,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear(),
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
