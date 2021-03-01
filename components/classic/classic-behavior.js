e3eca14ba1de4377d56bf927f7b2d2c681a955c9
liuyib <1656081615@qq.com>
2021-03-01 13:29:05 +0800

    pref: use 'hidden' to replace 'wx:if'

--- components/classic/classic-behavior.js
+++ components/classic/classic-behavior.js
@@ -1,11 +1,17 @@
 const classicBehavior = Behavior({
   properties: {
+    // 封面图片
     banner: {
       type: String,
     },
+    // 期刊内容
     content: {
       type: String,
     },
+    // 小程序组件中的 hidden 属性，用于控制组件的显示或隐藏
+    hidden: {
+      type: Boolean,
+    }
   },
 })
 
