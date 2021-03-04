a9fc090ea293194c4e2ee7500311c8f1f0d7d442
liuyib <1656081615@qq.com>
2021-03-04 17:48:33 +0800

    chore: add button cmp

---
+++ components/button/index.js
@@ -0,0 +1,28 @@
+// components/button/index.js
+Component({
+  options: {
+    multipleSlots: true,
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+    },
+    isSlot: {
+      type: Boolean,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+})
